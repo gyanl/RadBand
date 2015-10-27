f461c6fb93a34adf498ba87afeedefa837f31a83
gyanl <gyanlakhwani@gmail.com>
2015-10-27 22:46:45 +0530

    Add Radjective option

--- RadBand.js
+++ RadBand.js
@@ -12,10 +12,46 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest tasks at the top
+
+return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+  Template.body.events({
 
+    "submit .new-task": function (event) {
+
+      // Prevent default browser form submit
+
+      event.preventDefault();
+
+
+
+      // Get value from form element
+
+      var text = event.target.text.value;
+
+
+
+      // Insert a task into the collection
+
+      Tasks.insert({
+
+        text: text,
+
+        createdAt: new Date() // current time
+
+      });
+
+
+
+      // Clear form
+
+      event.target.text.value = "";
+
+    }
+
+  });
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
